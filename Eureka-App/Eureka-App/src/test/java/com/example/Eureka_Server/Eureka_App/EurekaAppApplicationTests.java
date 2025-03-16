1dfbd2d40c04f4053a0a7dc3a49405725d9db6ed
yash <gyash986@gmail.com>
2025-03-16 13:57:48 +0530

    Eureka server

---
+++ Eureka-App/Eureka-App/src/test/java/com/example/Eureka_Server/Eureka_App/EurekaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eureka_Server.Eureka_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
